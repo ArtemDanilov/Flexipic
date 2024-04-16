a2db0f01570c114488b4998cd7ddb8d8da4a3d6e
ArtemDanilov <artemdanilow95@gmail.com>
2024-04-16 11:16:36 +0200

    Add Flexipic class and configs into service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,11 +3,31 @@
 namespace Artemdanilov\Flexipic;
 
 use Statamic\Providers\AddonServiceProvider;
+use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
+    protected $tags = [
+        \Artemdanilov\Flexipic\Tags\Flexipic::class
+    ];
+
     public function bootAddon()
     {
-        //
+        Statamic::afterInstalled(function ($command) {
+            $command->call('vendor:publish', ['--tag' => 'flexipic']);
+        });
+
+        $this->bootAddonConfig();
+    }
+
+    protected function bootAddonConfig(): self
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/flexipic.php', 'statamic.flexipic');
+
+        $this->publishes([
+            __DIR__.'/../config/flexipic.php' => config_path('statamic/flexipic.php'),
+        ], 'flexipic');
+
+        return $this;
     }
 }
