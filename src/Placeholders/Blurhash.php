48663e1046c2e42f91eeac7b16968e34befccb3c
ArtemDanilov <artemdanilow95@gmail.com>
2024-07-04 13:21:02 +0200

    Fix blurhash "null bytes" error

--- src/Placeholders/Blurhash.php
+++ src/Placeholders/Blurhash.php
@@ -17,18 +17,18 @@ class Blurhash
         $this->width = $width;
         $this->height = $height;
     }
-    
+
     public function create()
     {
         $hash = $this->encode($this->image);
-        
+
         return $this->decode($hash);
     }
 
     protected function encode($image = null)
-    {   
+    {
         if ($image instanceof AssetContract) {
-            $image = $image->contents();
+            $image = $image->resolvedPath();
         }
 
         return BlurHashFacade::encode($image);
@@ -37,15 +37,15 @@ class Blurhash
     protected function decode($encodedString = null)
     {
         $image = BlurHashFacade::decode($encodedString, $this->width, $this->height);
-        
+
         $tempFile = tempnam(sys_get_temp_dir(), 'img');
-        
+
         imagejpeg($image, $tempFile, 90);
-        
+
         $imageContents = file_get_contents($tempFile);
-        
+
         unlink($tempFile);
-        
+
         return $imageContents;
     }
 }
